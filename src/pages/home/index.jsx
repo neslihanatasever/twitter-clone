cfd5f97ec5417474ea9fa6148469b52b6445df2f
neslihan <neslihan_atasvr111@outlook.com>
2023-10-03 22:44:32 +0300

    complate layout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MainLayout from '~/layout/main'
 
 export default function Home() {
   return (
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "~/layout/main";
 import Explore from "~/pages/explore";
 import Home from "~/pages/home";
+import NotFound from "~/pages/not-found";
 import Notifications from "~/pages/notifications";
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+        {
+            index: true,
+            element: <Home />
+        },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
+      {
+        path: "notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
- const routes = createBrowserRouter ([
-    {
-        path: '/',
-        element: <Home />
-    },
-    {
-        path: '/explore',
-        element: <Explore />
-    },
-    {
-        path: '/notifications',
-        element: <Notifications />
-    }
-])
-
-export default routes
+export default routes;
