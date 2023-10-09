93a5886cad9b9b3d66c03777cad5dede288f44b3
nesly <neslihan_atasvr111@outlook.com>
2023-10-09 10:13:06 +0300

    sol sidebar tamamlanmak üzere

--- src/layout/main/sidebar/menu/index.jsx
+++ src/layout/main/sidebar/menu/index.jsx
@@ -11,14 +11,23 @@ export default function Menu() {
             <div
               className={classNames(
                 "p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]",
-                { "font-bold": isActive })}>
-              {!isActive && menu.icon.passive}
-              {isActive && menu.icon.active}
+                { "font-bold": isActive }
+              )}
+            >
+              <div className="w-[26.25px] h-[26.25px] relative">
+                {menu.notification && (
+                  <span className="w-[18px] h-[18px] rounded-full bg-[#1d9bf0] absolute -top-1.5 -right-1 flex items-center justify-center text-[11px]">
+                  {menu.notification}
+                </span>
+                )}
+                {!isActive && menu.icon.passive}
+                {isActive && menu.icon.active}
+              </div>
               <div className="pr-4 text-xl">{menu.title}</div>
             </div>
           )}
         </NavLink>
       ))}
     </nav>
-  )
+  );
 }
